f220b5d81ab6c5f3684df33da372d1504b84a906
NicolaTeora <nikxodia@gmail.com>
2024-02-07 16:56:26 +0100

    fix : App.vue -> AppTitle.vue

---
+++ src/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data (){
+    return{
+      title: 'La mia prima app con Vite!'
+    }
+  },
+
+  methods(){},
+
+  computed(){}
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style>
+
+</style>
